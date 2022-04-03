632a5522b6a2a82d27d7e4997e42e179694a4491
jonhunn <69512197+jonhunn@users.noreply.github.com>
2022-04-03 18:27:16 -0400

    Add files via upload
    initial backup

---
+++ computerPing.ps1
@@ -0,0 +1,31 @@
+﻿# This script pings a list of computers
+# and pushes them to a modified open source ping program
+# called vmPing https://github.com/R-Smith/vmPing
+
+
+
+$list = ''
+$finalList = ''
+$pathVMping = 'Software\vmPing\vmping.exe'
+$pathFMlist = 'C:\sns\fmList.txt'
+
+function killVMping{
+	$task = Get-Process -name vmping -erroraction SilentlyContinue
+	if ($task){
+         $task | Stop-Process -Force
+         }
+}
+function getOffline{
+		Get-Content $pathFMlist| ForEach-Object {
+			if(Test-Connection -computer $_ -count 1 -Quiet){
+				#purposefully left blank in case something is needed in the future
+			}else{
+				$list = $_
+				$finalList = $finalList +" "+$list
+			}
+		}
+		killVMping
+		Start-Process $pathVMping $finalList
+}
+
+getOffline
